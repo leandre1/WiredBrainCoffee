7117ed0a585f3b152d63571c64660bab35875841
leandre bizagwira <lbizagwira@gmail.com>
2019-08-25 17:49:44 +0100

    Implemented help command

--- WiredBrainShopTool.CoffeeTool/WiredBrainShopTool.CoffeeTool/Program.cs
+++ WiredBrainShopTool.CoffeeTool/WiredBrainShopTool.CoffeeTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess.Model;
 
 namespace WiredBrainShopTool.CoffeeTool
 {
@@ -7,6 +8,38 @@ namespace WiredBrainShopTool.CoffeeTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Info Tool!");
+
+            Console.WriteLine("Type help to see available coffee shop, and quit to exit!");
+
+            var data = new CoffeeShopDataProvider();
+            var line = Console.ReadLine();
+            bool valid = true;
+            while (valid)
+            {
+               
+
+                if (string.Equals("help",line, StringComparison.OrdinalIgnoreCase))
+                {
+                    var coffeeShops = data.LoadCoffeeShop();
+                    Console.WriteLine("Available coffee shops!");
+                    foreach (var shop in coffeeShops)
+                    {
+                        Console.WriteLine($"> {shop.Locations}");
+
+                    }
+                    Console.ReadLine();
+                    
+                }
+                
+               
+            }
+          
+
+
+
+
+
+
         }
     }
 }
