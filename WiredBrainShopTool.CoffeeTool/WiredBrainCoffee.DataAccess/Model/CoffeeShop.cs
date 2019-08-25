4c21503997ac334d16b54f9538790636cbf5f563
leandre bizagwira <lbizagwira@gmail.com>
2019-08-25 17:48:41 +0100

    Added CoffeeShop

---
+++ WiredBrainShopTool.CoffeeTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+   public class CoffeeShop
+    {
+        public string Locations { get; set; }
+        public int BeansInStockInKgs { get; set; }
+    }
+}
---
+++ WiredBrainShopTool.CoffeeTool/WiredBrainCoffee.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShop()
+        {
+            yield return new CoffeeShop() { BeansInStockInKgs = 100, Locations = "Frankfurt" };
+            yield return new CoffeeShop() { BeansInStockInKgs = 50, Locations = "Brussels" };
+            yield return new CoffeeShop() { BeansInStockInKgs = 150, Locations = "Paris" };
+        }
+    }
+}
