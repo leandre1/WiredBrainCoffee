797fce826491af33a50fe7c6f47fbba9ec67e9ea
leandre bizagwira <lbizagwira@gmail.com>
2019-08-25 10:05:06 +0100

    Add project

---
+++ WiredBrainShopTool.CoffeeTool/WiredBrainShopTool.CoffeeTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainShopTool.CoffeeTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Info Tool!");
+        }
+    }
+}
